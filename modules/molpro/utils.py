c446278837b55137eeed52c12e9dbcabb9935a6d
Robert Adam <dev@robert-adam.de>
2022-03-24 16:49:39 +0100

    Added functions to parse iteration tables

--- modules/molpro/utils.py
+++ modules/molpro/utils.py
@@ -2,8 +2,12 @@ from typing import Optional
 from typing import List
 from typing import Iterator
 from typing import Any
+from typing import Dict
+from typing import Union
+from typing import Set
 
 from molpro import OutputFormatError
+from molpro import IterationTable
 
 
 def consume(content: str, prefix: str = "", gobble_until: Optional[str] = None, gobble_from: Optional[str] = None, gobble_after: Optional[str] = None, strip=False,
@@ -86,3 +90,73 @@ def skip_to(content_list: List[str], indexIt: Iterator[int], contains: Optional[
 def iterate_to(it: Iterator[Any], to: Any):
     while next(it) != to:
         pass
+
+
+def process_columns(line: str, types: List[type] = [], delimiter: Optional[str] = None, expected_column_count: int = -1, format_exception: type =
+                    OutputFormatError, remove_empty: bool = False) -> List[Any]:
+    """Parses the given line by splitting it into different columns and optionally transforming each column to a given type"""
+    columns = line.split() if delimiter is None else line.split(delimiter)
+
+    if remove_empty:
+        columns = [x for x in columns if x != ""]
+
+    if expected_column_count >= 0 and len(columns) != expected_column_count:
+        raise format_exception("Expected %d columns but got %d" % (
+            expected_column_count, len(columns)))
+
+    if len(types) > 0:
+        if len(columns) != len(types):
+            raise format_exception("Amount of types (%d) doesn't match with amount of columns (%d)" % (
+                len(types), len(columns)))
+
+        for i in range(len(columns)):
+            if types[i] is float:
+                # Make sure we convert from Fortran-style scientific notation to regular one
+                columns[i] = columns[i].replace("D", "E")
+
+            columns[i] = types[i](columns[i])
+
+    return columns
+
+
+def parse_iteration_table(lines: List[str], lineIt: Iterator[int], format_exception: type = OutputFormatError, col_sep: Optional[str] = None,
+                          col_types: List[type] = [], substitutions: Dict[str, str] = {}, del_cols: Set[Union[int, str]] = set()) -> IterationTable:
+    headerLine = lines[next(lineIt)]
+
+    for currentSub in substitutions:
+        headerLine = headerLine.replace(currentSub, substitutions[currentSub])
+
+    headers = process_columns(
+        headerLine, delimiter=col_sep, remove_empty=True, format_exception=format_exception)
+
+    if len(headers) == 0:
+        raise format_exception(
+            "Can't parse iteration table for empty header line")
+    if len(col_types) != 0 and len(headers) != len(col_types):
+        raise format_exception("The amount of found column headers (%d) does not match the expected one (%d)" % (
+            len(headers), len(col_types)))
+
+    iterations = []
+
+    for i in lineIt:
+        if lines[i].strip() == "":
+            break
+
+        iterations.append(process_columns(lines[i], delimiter=col_sep, remove_empty=True, expected_column_count=len(headers),
+                                          format_exception=format_exception, types=col_types))
+
+    # Remove specified columns. First convert column header names into column indices though
+    del_cols = {
+        x if type(x) is int else headers.index(x) for x in del_cols}
+    if any(not type(n) is int or n < 0 for n in del_cols):
+        raise format_exception(
+            "Encountered invalid column index or column header ID (specified for deletion)")
+
+    headers = [v for i, v in enumerate(headers) if not i in del_cols]
+    for i in range(len(iterations)):
+        iterations[i] = [v for i, v in enumerate(iterations[i]) if not i in del_cols]
+
+
+    table = IterationTable(columnHeaders=headers, iterations=iterations)
+
+    return table
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,8 @@
 import unittest
 
 from molpro import IterationTable
+from molpro import utils
+
 
 class TestIterationTable(unittest.TestCase):
     def test_interface(self):
@@ -22,7 +24,6 @@ class TestIterationTable(unittest.TestCase):
         self.assertEqual(table.column(0), ["One", "Two", "Three"])
         self.assertEqual(table.column(2), [0.1, 0.2, 0.3])
 
-
     def test_named_column_lookup(self):
         """Test whether the different columns can be accessed by name"""
         headers = ["A", "B", "C"]
@@ -41,6 +42,53 @@ class TestIterationTable(unittest.TestCase):
             table.column("Doesn't exist")
 
 
+class TestUtils(unittest.TestCase):
+    def test_process_columns(self):
+        line = "A B C D"
+        expectedResult = ["A", "B", "C", "D"]
+
+        result = utils.process_columns(line)
+
+        self.assertEqual(result, expectedResult)
+
+        line = "A,B,C,,D"
+        expectedResult = ["A", "B", "C", "", "D"]
+        result = utils.process_columns(line, delimiter=",")
+        self.assertEqual(result, expectedResult)
+        result = utils.process_columns(line, delimiter=",", remove_empty=True)
+        expectedResult = ["A", "B", "C", "D"]
+        self.assertEqual(result, expectedResult)
+
+        line = "1 0.5 bla 1D-5 0.1E+7"
+        expectedResult = [1, 0.5, "bla", 1E-5, 0.1E7]
+        result = utils.process_columns(
+            line, types=[int, float, str, float, float])
+        self.assertEqual(result, expectedResult)
+
+    def test_parse_iteration_table(self):
+        lines = [
+            "ITER           ETOT              DE          GRAD        DDIFF     DIIS  NEXP   TIME(IT)  TIME(TOT)  DIAG",
+            "1      -75.98578855     -75.98578855     0.00D+00     0.40D+00     0     0       0.01      0.07    start",
+            "2      -76.01569782      -0.02990927     0.37D-01     0.77D-01     1     0       0.00      0.07    diag",
+            "3      -76.02461371      -0.00891589     0.20D-01     0.26D-01     2     0       0.01      0.08    diag",
+        ]
+        lineIt = iter(range(len(lines)))
+
+        parsedTable = utils.parse_iteration_table(lines, lineIt, col_types=[int, float, float, float, float, int, int, float, float, str],
+                                                  del_cols={"ITER"})
+        expectedTable = IterationTable(columnHeaders=[
+                                       "ETOT", "DE", "GRAD", "DDIFF", "DIIS", "NEXP", "TIME(IT)", "TIME(TOT)", "DIAG"],
+                                       iterations=[
+                                           [-75.98578855, -75.98578855, 0,
+                                               0.4, 0, 0, 0.01, 0.07, "start"],
+                                           [-76.01569782, -0.02990927, 0.37E-1,
+                                               0.77E-1, 1, 0, 0, 0.07, "diag"],
+                                           [-76.02461371, -0.00891589, 0.2E-1,
+                                               0.26E-1, 2, 0, 0.01, 0.08, "diag"]
+        ])
+
+        self.assertEqual(parsedTable, expectedTable)
+
+
 if __name__ == "__main__":
     unittest.main()
-
