3af47598106053853f748fd86cc89be03cfb43bf
Robert Adam <dev@robert-adam.de>
2022-04-01 16:34:33 +0200

    Make sure run_tests.py produces proper exit code

--- run_tests.py
+++ run_tests.py
@@ -13,7 +13,9 @@ def main():
     test_suite = test_loader.discover(os.path.join(working_dir, "tests"), pattern="test_*.py")
 
     runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(test_suite)
+    result = runner.run(test_suite)
+
+    return 0 if len(result.errors) == 0 and len(result.failures) == 0 else 1
 
 
 if __name__ == "__main__":
